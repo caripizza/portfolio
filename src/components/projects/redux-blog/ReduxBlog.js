45b9062d6ee456689d09db5754114635573f9d2f
Cari Palazzolo <cari.pizza@gmail.com>
2019-04-20 21:37:04 -0700

    get rid of cyan background color, add gh icon to project1

--- src/components/projects/redux-blog/ReduxBlog.js
+++ src/components/projects/redux-blog/ReduxBlog.js
@@ -4,6 +4,7 @@ import styles from './ReduxBlog.css';
 import userDetail from '../../../../assets/redux-blog/users.png';
 import searchPosts from '../../../../assets/redux-blog/search-posts.png';
 import longestPosts from '../../../../assets/redux-blog/longest-posts.png';
+import github from '../../../../assets/github.png';
 
 const ReduxBlog = () => {
   return (
@@ -20,12 +21,17 @@ const ReduxBlog = () => {
             </a>
           </span>
           <br/>
+          <a href="https://github.com/caripizza/redux-blog"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <img src={github} alt="github" className={styles.github}/>
+          </a>
           <p>Featuring a search component allowing the user to filter through lorem ipsum blog posts:</p>
-          <img src={searchPosts} alt="search-posts"/>
+          <img src={searchPosts} alt="search-posts" className={styles.screenshots}/>
           <p>The Homepage includes the top 5 longest posts based on length:</p>
-          <img src={longestPosts} alt="longest-posts"/>
+          <img src={longestPosts} alt="longest-posts" className={styles.screenshots}/>
           <p>Clicking on a Post or User takes you to the detail page:</p>
-          <img src={userDetail} alt="user-detail"/>
+          <img src={userDetail} alt="user-detail" className={styles.screenshots}/>
 
           <Link to="/projects">Back</Link>
           <br/>
