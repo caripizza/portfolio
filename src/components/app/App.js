ffcceeaa425ed5b1f3f4543a21439eb99c332fe0
Cari Palazzolo <cari.pizza@gmail.com>
2019-05-11 17:12:02 -0700

    add loading spinner for initial site rendering

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,4 @@
-import React, {
-  // Component,
-  PureComponent
-} from 'react';
+import React, { PureComponent } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
@@ -9,83 +6,60 @@ import { ROUTES } from '../../routes/routes';
 import './App.css';
 import 'normalize.css';
 import ScrollToTop from '../../containers/ScrollToTop';
-// import preloader from '../../../assets/preloader.gif';
-
-const loadDelay = () => {
-  return new Promise((resolve) => setTimeout(() => resolve(), 200));
-};
 
 export default class App extends PureComponent {
-// export default class App extends Component {
-  state = {
-    loading: true
-  };
-
-  componentDidMount() {
-    loadDelay()
-      .then(() => {
-        this.setState({ loading: false });
-      });
-  }
-
   render() {
-    const { loading } = this.state;
+    const spinner = document.getElementById('spinner');
 
-    if(loading) {
-      return null;
-      // return (
-      //   <div>
-      //     <img src={preloader}/>
-      //   </div>
-      // );
+    if(spinner && !spinner.hasAttribute('hidden')) {
+      spinner.setAttribute('hidden', 'true');
     }
-    else {
-      return (
-        <Router>
-        < >
-          <Header/>
-          <ScrollToTop>
-            <Switch>
-              <Route exact path={ROUTES.HOME.path}
-                component={ROUTES.HOME.Component}
-              />
-              <Route exact path={ROUTES.WORK.path}
-                component={ROUTES.WORK.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS.path}
-                component={ROUTES.PROJECTS.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_HOMEBREWDOG.path}
-                component={ROUTES.PROJECTS_HOMEBREWDOG.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_MEME_GENERATOR.path}
-                component={ROUTES.PROJECTS_MEME_GENERATOR.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_SIM_ALCHEMY.path}
-                component={ROUTES.PROJECTS_SIM_ALCHEMY.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_REDUX_BLOG.path}
-                component={ROUTES.PROJECTS_REDUX_BLOG.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_POKEDEX.path}
-                component={ROUTES.PROJECTS_POKEDEX.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_ASYNC_RM.path}
-                component={ROUTES.PROJECTS_ASYNC_RM.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_Q_BOT.path}
-                component={ROUTES.PROJECTS_Q_BOT.Component}
-              />
-              <Route exact path={ROUTES.PROJECTS_TCP_CHAT_ROOM.path}
-                component={ROUTES.PROJECTS_TCP_CHAT_ROOM.Component}
-              />
-              <Redirect to={ROUTES.HOME.path} />
-            </Switch>
-          </ScrollToTop>
-          <Footer/>
-        </>
-        </Router>
-      );
-    }
+
+    return (
+      <Router>
+      < >
+        <Header/>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path={ROUTES.HOME.path}
+              component={ROUTES.HOME.Component}
+            />
+            <Route exact path={ROUTES.WORK.path}
+              component={ROUTES.WORK.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS.path}
+              component={ROUTES.PROJECTS.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_HOMEBREWDOG.path}
+              component={ROUTES.PROJECTS_HOMEBREWDOG.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_MEME_GENERATOR.path}
+              component={ROUTES.PROJECTS_MEME_GENERATOR.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_SIM_ALCHEMY.path}
+              component={ROUTES.PROJECTS_SIM_ALCHEMY.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_REDUX_BLOG.path}
+              component={ROUTES.PROJECTS_REDUX_BLOG.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_POKEDEX.path}
+              component={ROUTES.PROJECTS_POKEDEX.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_ASYNC_RM.path}
+              component={ROUTES.PROJECTS_ASYNC_RM.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_Q_BOT.path}
+              component={ROUTES.PROJECTS_Q_BOT.Component}
+            />
+            <Route exact path={ROUTES.PROJECTS_TCP_CHAT_ROOM.path}
+              component={ROUTES.PROJECTS_TCP_CHAT_ROOM.Component}
+            />
+            <Redirect to={ROUTES.HOME.path} />
+          </Switch>
+        </ScrollToTop>
+        <Footer/>
+      </>
+      </Router>
+    );
   }
 }
