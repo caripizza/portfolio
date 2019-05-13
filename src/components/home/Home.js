3048193fe1bfcafde6c466b5058cacf0e330a913
Cari Palazzolo <cari.pizza@gmail.com>
2019-05-13 08:14:27 -0700

    useState to load spinner before Home logo img, styles

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,36 +1,41 @@
-import React from 'react';
-import logo from '../../../assets/caripizza.png';
+import React, {
+  useState,
+  useEffect
+} from 'react';
+import logo from '../../../assets/caripizza_tr.png';
 import styles from './Home.css';
+import preloader from '../../../assets/spinnerLoad.gif';
 
-const welcomeMsg1 = `
-  Welcome! I'm Cari, a Full Stack JavaScript developer, musician, and graphic designer.
-`;
-const welcomeMsg2 = `
-  After a decade performing and teaching music, I started dabbling in freelance website design and digital marketing for small businesses.
-  I made my way up to the Pacific Northwest and joined the tech scene as a support agent, later working as a QA tester for a small software company where I fell in love with the software development cycle and decided to take the plunge into coding.
-`;
-
-const welcomeMsg3 = `
-  In early 2019, I completed a 6 month intensive training program in full stack JavaScript, focusing in front-end website design as well as DevOps, managing the release flow.
-  I made this site with React, styled it with PostCSS, setup continuous integration with Travic CI, and deployed it to Netlify. I'm a self taught multi-instrumentalist and audio engineer, fierce problem solver and collaborative team player, and I love learning new technologies.
-`;
+const welcomeMsg1 = 'Welcome! I\'m Cari, a Full Stack JavaScript developer, musician, and graphic designer.';
+const welcomeMsg2 = 'After a decade performing and teaching music, I started dabbling in freelance website design and digital marketing for small businesses. I made my way up to the Pacific Northwest and joined the tech scene as a support agent, later working as a QA tester for a small software company where I fell in love with the software development cycle and decided to take the plunge into coding.';
+const welcomeMsg3 = 'In early 2019, I completed a 6 month intensive training program in full stack JavaScript, focusing in front-end website design as well as DevOps, managing the release flow. I made this site with React, styled it with PostCSS, setup continuous integration with Travic CI, and deployed it to Netlify. I\'m a self taught multi-instrumentalist and audio engineer, fierce problem solver and collaborative team player, and I love learning new technologies.';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 300);
+  }, []);
+
   return (
     <>
 
       <main className={styles.Home}>
 
         <section className={styles.welcomeSect}>
-          <img src={logo} alt="logo" className={styles.slide}/>
-          <p className={styles.welcome}>{welcomeMsg1}</p>
-          <p>{welcomeMsg2}</p>
-          <p>{welcomeMsg3}</p>
+          <div className={`${styles.imgDiv} ${styles.slide}`}>
+            <img src={loading ? preloader : logo} alt="logo" className={styles.fadeIn}/>
+            {/* <img src={logo} alt="logo" className={styles.fadeIn}/> */}
+          </div>
+          <p className={`${styles.highlight} ${styles.pWelcome1}`}>
+            {welcomeMsg1}
+          </p>
+          <p className={styles.welcomeParagraphs}>{welcomeMsg2}</p>
+          <p className={styles.welcomeParagraphs}>{welcomeMsg3}</p>
         </section>
 
         <section className={styles.half}>
           <article className={styles.skills}>
-            <h2><strong>Skills</strong></h2>
+            <h2 className={styles.highlight}><strong>Skills</strong></h2>
             <ul>
               <li><span><strong>Design:</strong> UI/UX, Mobile-first/responsive design principles</span></li>
               <li><span><strong>BDD/TDD:</strong> Selenium, Protractor, Jenkins,  Jasmine, Jest, Enzyme, Mocha/Chai</span></li>
@@ -46,7 +51,9 @@ const Home = () => {
           </article>
 
           <article className={styles.education}>
-            <h2><strong>Academic Background</strong></h2>
+            <h2 className={styles.highlight}>
+              <strong>Academic Background</strong>
+            </h2>
             <ul>
               <li><span><strong>Advanced Software Development in Full Stack JavaScript:</strong><br/>Alchemy Code Lab (Portland, 2018-2019)</span></li>
               <li className={styles.detail}>
@@ -63,7 +70,7 @@ const Home = () => {
         </section>
 
         <section className={styles.languages}>
-          <p>JavaScript</p><p>HTML/HTML5</p><p>CSS/CSS3</p><p>SQL</p><p>Python</p>
+          <p>JavaScript</p><p>HTML/HTML5</p><p>CSS/CSS3</p><p>SQL</p>
         </section>
 
       </main>
