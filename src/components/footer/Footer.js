f160c0c5ad6eff64a23c357973e7099ed1c3c7ac
Cari Palazzolo <cpalazzolo@renewfinancial.com>
2020-03-21 15:51:06 -0700

    add footer copy

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styles from './Footer.css';
 
+const copyright = `© ${new Date().getFullYear()} Cari Palazzolo`;
+const linkedinLink = (
+  <a href="https://linkedin.com/in/cari-palazzolo"
+    target="_blank" rel="noopener noreferrer"
+  >
+    Linkedin
+  </a>
+);
+const aboutLink = (
+  <a href="https://caripalazzolo.com/about" target="_blank" rel="noopener noreferrer">About</a>
+);
+
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
       <ul>
         <li>
-          <p>{`© ${new Date().getFullYear()} Cari Palazzolo`}</p>
+          <p>
+            {copyright} | {linkedinLink} | {aboutLink}
+          </p>
         </li>
       </ul>
     </footer>
